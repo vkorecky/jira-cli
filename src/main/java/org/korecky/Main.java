4b7eef42f4282989dea83f204bffb1d4920e6435
vkorecky <vkorecky@korecky.org>
2023-11-17 13:48:36 +0100

    Addresses #1 - Implements to establish a connection to the JIRA server

--- src/main/java/org/korecky/Main.java
+++ src/main/java/org/korecky/Main.java
@@ -1,19 +1,55 @@
 package org.korecky;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Replace these with your JIRA server details and credentials
+        String jiraServerUrl = "https://your-jira-server.com";
+        String username = "your-username";
+        String password = "your-password";
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // JIRA REST API endpoint for retrieving issue details (replace 'KEY' with the actual issue key)
+        String issueKey = "ABC-12345";
+        String apiUrl = jiraServerUrl + "/rest/api/2/issue/" + issueKey;
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        // Create HttpClient
+        HttpClient httpClient = HttpClients.createDefault();
+
+        // Create HTTP GET request
+        HttpGet getRequest = new HttpGet(apiUrl);
+
+        // Set authentication credentials
+        getRequest.setHeader("Authorization", "Basic " + java.util.Base64.getEncoder().encodeToString((username + ":" + password).getBytes()));
+
+        try {
+            // Execute the request
+            HttpResponse response = httpClient.execute(getRequest);
+
+            // Check for a successful response (HTTP 200)
+            if (response.getStatusLine().getStatusCode() == 200) {
+                // Read and print the response
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } else {
+                System.err.println("Error: " + response.getStatusLine().getReasonPhrase());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the HttpClient
+            httpClient.getConnectionManager().shutdown();
         }
     }
 }
